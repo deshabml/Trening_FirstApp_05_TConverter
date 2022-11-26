4ebda18c635268167681f26e0198e7f599c4f41c
deshabml <deshab@yandex.ru>
2022-11-26 12:46:18 +0300

    create app completed

--- TConverter/ViewController.swift
+++ TConverter/ViewController.swift
@@ -10,10 +10,20 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var cenlinusLabel: UILabel!
-
     @IBOutlet weak var fahrenheitLabel: UILabel!
-
+    @IBOutlet weak var slider: UISlider! {
+        didSet {
+            slider.maximumValue = 100
+            slider.minimumValue = 0
+            slider.value = 0
+        }
+    }
+    
     @IBAction func sliderChanged(_ sender: UISlider) {
+        let temperaturCelsius = Int(round(sender.value))
+        cenlinusLabel.text = "\(temperaturCelsius)ºC"
+        let temperaturFahreniet = temperaturCelsius * 9 / 5 + 32
+        fahrenheitLabel.text = "\(temperaturFahreniet)ºF"
     }
     
 }
